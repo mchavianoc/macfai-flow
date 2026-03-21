84cf25790e63ba64ecc5ec7837f01ad1a7aa9b78
Miguel Chaviano <mchavianoc@gmail.com>
2026-03-21 13:07:19 -0400

    add static

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path('dashboard/', include('client_dashboard.urls')), 
     path('webhooks/', include('webhooks.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
